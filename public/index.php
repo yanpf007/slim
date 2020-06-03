58c638acaf515bb137bb8e0b2e1aeeaf6bb670a2
yanpengfei <930896170@qq.com>
2020-06-03 14:28:24 +0800

    slim initialize

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+$app = AppFactory::create();
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('<a href="/hello/world">Try /hello/world</a>');
+    return $response;
+});
+$app->run();
